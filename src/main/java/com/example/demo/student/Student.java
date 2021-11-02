0099e820f228f87d3e4dc6a9883826bc66f95635
Miguel Canessa <canessaalvamiguel@gmail.com>
2021-11-02 00:39:01 -0500

    Add annotations to student entity

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -1,8 +1,21 @@
 package com.example.demo.student;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private String email;
@@ -26,6 +39,10 @@ public class Student {
         this.email = email;
         this.dob = dob;
         this.age = age;
+    }
+
+    public Student() {
+
     }
 
     public Long getId() {
