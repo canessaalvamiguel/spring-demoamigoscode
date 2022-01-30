21d7f42fe041808fd97778fc40d6abbb97244cd4
Miguel Canessa <canessaalvamiguel@gmail.com>
2022-01-29 19:21:22 -0500

    Setting age as transient

--- src/main/java/com/example/demo/student/StudentConfig.java
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -18,16 +18,14 @@ public class StudentConfig {
             Student miguel = new Student(
                     "Miguel",
                     "miguel@canessa.com",
-                    LocalDate.of(2000, JANUARY, 5),
-                    21
+                    LocalDate.of(2000, JANUARY, 5)
             );
 
             Student alex = new Student(
                     2L,
                     "Alex",
                     "alex@canessa.com",
-                    LocalDate.of(2004, JANUARY, 15),
-                    25
+                    LocalDate.of(2004, JANUARY, 15)
             );
 
             repository.saveAll(
